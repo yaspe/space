f6b33f80b3a31ed5aa724285c158fa4bffa42b64
yaspe <vasily.matveev@gmail.com>
2016-10-22 17:26:00 +0300

    cool ship

--- src/space/main.go
+++ src/space/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	enginePower = 0.005
+	enginePower = 0.05
 	maxSpeed    = 15
 	worldSize   = 480
 )
@@ -17,23 +17,18 @@ func main() {
 	defer window.Destroy()
 	renderer, _ := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	defer renderer.Destroy()
-	image, _ := sdl.LoadBMP("/Users/ya-spe/Downloads/space_img.bmp")
-	defer image.Free()
-	texture, _ := renderer.CreateTextureFromSurface(image)
-	defer texture.Destroy()
-
-	src := sdl.Rect{0, 0, 64, 64}
 
 	s := &Ship{}
+	s.position.x = 50
+	s.position.y = 50
 
 	for {
 		s.Process()
-		s.engineMain()
+		s.EngineMain()
 		renderer.Clear()
-		dst := sdl.Rect{int32(s.position.x), int32(s.position.y), 64, 64}
-		renderer.Copy(texture, &src, &dst)
+		s.Draw(renderer)
 		renderer.Present()
-		sdl.Delay(100)
+		sdl.Delay(50)
 	}
 }
 
@@ -59,9 +54,47 @@ type Ship struct {
 	AbstractObject
 }
 
-func (s *Ship) engineMain() {
-	s.acceleration.x = enginePower * math.Sin(s.rotation)
-	s.acceleration.y = enginePower * math.Cos(s.rotation)
+func (s *Ship) Draw(renderer *sdl.Renderer) {
+	const half_size = 32
+
+	s.rotation = 1.3
+	renderer.SetDrawColor(0, 255, 255, 255)
+	renderer.DrawLine(
+		int(s.position.x),
+		int(s.position.y),
+		int(s.position.x+half_size*math.Cos(s.rotation)),
+		int(s.position.y+half_size*math.Sin(s.rotation)),
+	)
+	renderer.DrawLine(
+		int(s.position.x),
+		int(s.position.y),
+		int(s.position.x+half_size*math.Cos(s.rotation+3*math.Pi/4)),
+		int(s.position.y+half_size*math.Sin(s.rotation+3*math.Pi/4)),
+	)
+	renderer.DrawLine(
+		int(s.position.x),
+		int(s.position.y),
+		int(s.position.x-half_size*math.Sin(s.rotation+3*math.Pi/4)),
+		int(s.position.y+half_size*math.Cos(s.rotation+3*math.Pi/4)),
+	)
+	renderer.DrawLine(
+		int(s.position.x+half_size*math.Cos(s.rotation)),
+		int(s.position.y+half_size*math.Sin(s.rotation)),
+		int(s.position.x+half_size*math.Cos(s.rotation+3*math.Pi/4)),
+		int(s.position.y+half_size*math.Sin(s.rotation+3*math.Pi/4)),
+	)
+	renderer.DrawLine(
+		int(s.position.x+half_size*math.Cos(s.rotation)),
+		int(s.position.y+half_size*math.Sin(s.rotation)),
+		int(s.position.x-half_size*math.Sin(s.rotation+3*math.Pi/4)),
+		int(s.position.y+half_size*math.Cos(s.rotation+3*math.Pi/4)),
+	)
+	renderer.SetDrawColor(0, 0, 0, 0)
+}
+
+func (s *Ship) EngineMain() {
+	s.acceleration.x = enginePower * math.Cos(s.rotation)
+	s.acceleration.y = enginePower * math.Sin(s.rotation)
 }
 
 func (s *AbstractObject) Process() {
@@ -80,5 +113,15 @@ func (s *AbstractObject) Process() {
 	s.position.x += s.speed.x
 	s.position.y += s.speed.y
 
-	//if s.position.x
+	if s.position.x > worldSize {
+		s.position.x = 0
+	} else if s.position.x < 0 {
+		s.position.x = worldSize
+	}
+
+	if s.position.y > worldSize {
+		s.position.y = 0
+	} else if s.position.y < 0 {
+		s.position.y = worldSize
+	}
 }
